47d343df18f686d21b99cc7aef282925cd1f5bae
dsys <dsyslove@163.com>
2023-10-23 20:02:58 +0800

    添加message proto js文件

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 const msgId=ref('0')
-let websocket=new WebSocket("ws://localhost:9000/ws")
-websocket.onopen=function(){
-
+var websocket;
+const wsOpen=()=>{
+  console.log("ws open")
 }
+onMounted(()=>{
+  websocket=new WebSocket("ws://localhost:9000/ws")
+  websocket.onopen=wsOpen()
+})
 </script>
 
 <template>
