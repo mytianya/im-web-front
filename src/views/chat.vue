02549f800221d955f0bea7fe91b865063801fef9
dsys <dsyslove@163.com>
2023-10-29 20:51:51 +0800

    增加用户登录接口

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -1,18 +1,33 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-const msgId=ref('0')
-var websocket;
-const wsOpen=()=>{
+import { ref, onMounted } from 'vue'
+const msgId = ref('0')
+var ws;
+var fromuid='dsys'
+const wsOpen = () => {
   console.log("ws open")
+  ws.binaryType = "arraybuffer";
+  var msg = new proto.Msg();
+  var head = new proto.Head();
+  head.setMsgtype(1004);
+  head.setMsgid(1000000);
+  head.setFromuid(fromuid);
+  head.setTimestamp(new Date().getTime())
+  msg.setHead(head);
+  msg.setBody(
+    '{"clientType":"user","clientSecret":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImRzeXMiLCJleHAiOjE2OTc4ODg1ODJ9.nnYGM91C-lssB4Cfe_ivkibJlRJztbNCFcOOG7Xyf5A"}'
+  );
+  console.log(msg.serializeBinary())
+  // Web Socket 已连接上，使用 send() 方法发送数据
+  ws.send(msg.serializeBinary());
 }
-onMounted(()=>{
-  websocket=new WebSocket("ws://localhost:9000/ws")
-  websocket.onopen=wsOpen()
+onMounted(() => {
+  ws = new WebSocket("ws://localhost:8000/ws")
+  ws.onopen = wsOpen
 })
 </script>
 
 <template>
-  <div>{{msgId}}</div>
+  <div>{{ msgId }}</div>
 </template>
 
 <style scoped></style>
