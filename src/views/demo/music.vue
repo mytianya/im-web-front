2fd9089eec2354c0150990f5797d65168538ab77
dsys <dsyslove@163.com>
2023-11-24 17:15:26 +0800

    测试歌词

--- src/views/demo/music.vue
+++ src/views/demo/music.vue
@@ -9,16 +9,19 @@ import 'APlayer/dist/APlayer.min.css';
 import APlayer from 'APlayer';
 import { nextTick } from 'vue';
 //let test1="https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
-let test2="http://192.168.22.90:8888/music/play?name=b.mp3"
-
+let musicName="悬溺";
+let musicUrl="http://192.168.22.90:8888/v1/music/audioSource?name=xn.mp3"
+let lrcUrl="http://192.168.22.90:8888/v1/music/lyricSource?name=xn.lrc";
 nextTick(()=>{
     let options={
     container:document.getElementById('aplayer'),
+    lrcType: 3,
     audio:[{
-        name:'name',
-        artist:'artist',
-        url:test2,
-        cover:'cover.jpg'
+        name:musicName,
+        artist:'未知者',
+        url:musicUrl,
+        cover:'cover.jpg',
+        lrc:lrcUrl
     }]
 }
     new APlayer(options);
